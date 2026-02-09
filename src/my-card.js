c603897456a9ca533d3aab0e76ede9aa7bee0d46
David Isidro <disidore56@gmail.com>
2026-02-08 23:52:20 -0500

    Apply card implementation using Lit components

--- src/my-card.js
+++ src/my-card.js
@@ -14,24 +14,60 @@ export class MyCard extends LitElement {
 
   constructor() {
     super();
-    this.title = "My card";
+    this.title = "Wanted";
+    this.subtitle = "DEAD OR ALIVE";
+    this.name = "CHOPPER";
+    this.bounty = "$1000";
+    this.img = "https://www.thepopverse.com/_next/image?url=https%3A%2F%2Fmedia.thepopverse.com%2Fmedia%2Ftony-tony-chopper-from-one-piece-t0mkr5fnn5tevppawlpiwuqtca.jpg&w=1280&q=75";
+    this.bg = "#D8C4A8";
   }
 
   static get styles() {
-    return css`
-      :host {
-        display: block;
-      }
-    `;
-  }
+  return css`
+    :host {
+      display: inline-block;
+    }
+
+    .card {
+      width: 200px;
+      padding: 16px;
+      border: 3px solid black;
+      border-radius: 8px;
+      text-align: center;
+      background-color: var(--bg, #D8C4A8);
+      box-sizing: border-box;
+    }
+
+    img {
+      width: 100%;
+      height: auto;
+      border: 2px solid black;
+      box-sizing: border-box;
+    }
+  `;
+}
+
 
   render() {
-    return html`<div>${this.title}</div>`;
+   return html`
+    <div class="card" style="--bg:${this.bg}">
+      <h2 class="card-title">${this.title}</h2>
+      <img src="${this.img}">
+      <p>${this.subtitle}</p>
+      <h3>${this.name}</h3>
+      <p>${this.bounty}</p>
+    </div>
+  `;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      img: { type: String },
+      subtitle: { type: String },
+      name: { type: String },
+      bounty: { type: String },
+      bg: { type: String },
     };
   }
 }
